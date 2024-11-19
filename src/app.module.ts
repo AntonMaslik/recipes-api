cf88ff55d53800d274aa0d1c870fae333b74ac33
AntonMaslik <maslik.fortech@gmail.com>
2024-11-19 13:09:21 +0300

    connect graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { GraphQLModule } from '@nestjs/graphql';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      playgroud: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
