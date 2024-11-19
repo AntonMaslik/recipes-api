346411b31dc7f36793a829c04d7b0e702871dd9b
AntonMaslik <maslik.fortech@gmail.com>
2024-11-19 17:39:08 +0300

    add auth signUp

--- src/modules/auth/resolves/auth.resolver.ts
+++ src/modules/auth/resolves/auth.resolver.ts
@@ -1,4 +1,14 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { User } from 'src/modules/users/models/user.model';
+import { SignUpDTO } from '../dto/sign-up.dto';
+import { AuthService } from '../auth.service';
 
 @Resolver('Auth')
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => User)
+  async signUp(@Args() signUpDTO: SignUpDTO): Promise<any> {
+    return this.authService.signUp(signUpDTO);
+  }
+}
