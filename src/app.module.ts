2703b0491e47f7dce1a427e5576df4ffd06b7f78
Anton <maslik.fortech@gmail.com>
2024-11-19 22:31:28 +0300

    arhitecture edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,16 @@ import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 
-import { DynamooseModule } from './aws/dynamoose/dynamoose.module';
+import { DynamooseModule } from './modules/dynamoose/dynamoose.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
-      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     DynamooseModule,
     UsersModule,
