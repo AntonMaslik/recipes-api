5d8cde7b57e5a47a280d1d177f75542aca96f664
AntonMaslik <maslik.fortech@gmail.com>
2024-11-25 15:35:22 +0300

    feature: add general methods in service

--- src/modules/recipes/recipes.service.ts
+++ src/modules/recipes/recipes.service.ts
@@ -1,6 +1,29 @@
+import { CreateRecipeDTO } from '@modules/recipes/dto/create-recipe.dto';
+import { UpdateRecipeDTO } from '@modules/recipes/dto/update-recipe.dto';
+import { RecipeModel } from '@modules/recipes/models/recipe.model';
+import { RecipesRepository } from '@modules/recipes/models/recipes.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RecipesService {
-    constructor() {}
+    constructor(private readonly recipesRepository: RecipesRepository) {}
+
+    async createRecipe(createRecipeDto: CreateRecipeDTO): Promise<RecipeModel> {
+        return this.recipesRepository.create(createRecipeDto);
+    }
+
+    async updateRecipe(
+        id: string,
+        updateRecipesDto: UpdateRecipeDTO,
+    ): Promise<RecipeModel> {
+        return this.recipesRepository.update(updateRecipesDto);
+    }
+
+    async deleteRecipeById(id: string): Promise<RecipeModel> {
+        return this.recipesRepository.softDelete(id);
+    }
+
+    async getRecipeById(id: string): Promise<RecipeModel> {
+        return this.recipesRepository.findById(id);
+    }
 }
