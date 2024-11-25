c17fc21c19f3247f28021a0de24a451fde444cc1
AntonMaslik <maslik.fortech@gmail.com>
2024-11-25 15:14:49 +0300

    feature: add dto

---
+++ src/modules/recipes/dto/create-recipe.dto.ts
@@ -0,0 +1,30 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+@InputType()
+export class CreateRecipeDTO {
+    @IsString()
+    @IsNotEmpty()
+    @Field()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Field()
+    body: string;
+
+    @IsArray()
+    @IsNotEmpty()
+    @Field()
+    ingriditens: string[];
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Field()
+    servingSize: number;
+
+    @IsString()
+    @IsNotEmpty()
+    @Field()
+    cookingTime: string;
+}
