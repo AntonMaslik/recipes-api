5fba7f326d56b15d0b66a494a9b60415bd7e5cda
AntonMaslik <maslik.fortech@gmail.com>
2024-11-29 14:02:35 +0300

    feature: rate init

---
+++ src/modules/rate/rate.module.ts
@@ -0,0 +1,10 @@
+import { dynamooseScheme } from '@app/config/db.schema';
+import { Module } from '@nestjs/common';
+import { DynamooseModule } from 'nestjs-dynamoose';
+
+@Module({
+    providers: [],
+    exports: [],
+    imports: [DynamooseModule.forFeature(dynamooseScheme)],
+})
+export class RateModule {}
