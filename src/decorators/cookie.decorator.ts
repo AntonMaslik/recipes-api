971320d6dcce32d9dff0e268937ccf6bc4fba4eb
Anton <maslik.fortech@gmail.com>
2024-12-02 00:27:08 +0300

    fix: cookie decorator replace any

--- src/decorators/cookie.decorator.ts
+++ src/decorators/cookie.decorator.ts
@@ -1,11 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { Request } from 'express';
 
 export const Cookie = createParamDecorator(
     (cookieName: string, context: ExecutionContext) => {
         const ctx: GqlExecutionContext = GqlExecutionContext.create(context);
 
-        const request: any = ctx.getContext().req;
+        const request: Request = ctx.getContext().req;
 
         return request.cookies[cookieName];
     },
