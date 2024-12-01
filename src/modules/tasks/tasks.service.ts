fccfd089c2de7fda7c38b1092a2613cfe174a309
Anton <maslik.fortech@gmail.com>
2024-12-02 02:24:09 +0300

    fix: move in generator

--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -3,7 +3,7 @@ import { RecipeModel } from '@app/modules/recipes/models/recipe.model';
 import { RecipesRepository } from '@app/modules/recipes/models/recipes.repository';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { InjectModel, Model, ScanResponse } from 'nestjs-dynamoose';
+import { InjectModel, Model } from 'nestjs-dynamoose';
 
 @Injectable()
 export class TasksService {
@@ -18,41 +18,56 @@ export class TasksService {
 
     @Cron(CronExpression.EVERY_2_HOURS)
     async handleCron() {
-        const recipes: ScanResponse<RecipeModel> =
+        const recipes: RecipeModel[] =
             await this.recipesRepository.findNonDeleted();
+        const ratedItems: RateModel[] = await this.rateModel.scan().exec();
 
-        const ratedItems: ScanResponse<RateModel> = await this.rateModel
-            .scan()
-            .exec();
-
-        if (ratedItems.length == 0 || recipes.length == 0) {
-            this.logger.log(`[INFO] Recipes or ratedItems not find!`);
+        if (ratedItems.length === 0 || recipes.length === 0) {
+            this.logger.log('[INFO] Recipes or ratedItems not found!');
 
             return;
         }
 
-        // O(n * m) - не очень хорошо, можно быстрее (Anton)
-        for (const recipe of recipes) {
-            let sumRatingForRecept: number = 0;
-            let countReceptItem: number = 0;
-
-            for (const rateItem of ratedItems) {
-                if (rateItem.recipeId == recipe.id) {
-                    sumRatingForRecept += rateItem.evaluation;
-                    countReceptItem += 1;
+        const ratingsMap = ratedItems.reduce(
+            (acc, rateItem) => {
+                if (!acc[rateItem.recipeId]) {
+                    acc[rateItem.recipeId] = [];
                 }
-            }
+                acc[rateItem.recipeId].push(rateItem.evaluation);
 
-            const avg: number = sumRatingForRecept / countReceptItem;
+                return acc;
+            },
+            {} as Record<string, number[]>,
+        );
 
+        for await (const recipe of this.getRecipeWithAverageRating(
+            recipes,
+            ratingsMap,
+        )) {
             await this.recipesRepository.update(recipe.id, {
-                rating: avg,
+                rating: recipe.avgRating,
             });
-
             await this.logger.log(
-                `Recept id: ${recipe.id}, his rating: ${avg}`,
+                `Recipe id: ${recipe.id}, new rating: ${recipe.avgRating}`,
                 'INFO',
             );
+        }
+    }
+
+    async *getRecipeWithAverageRating(
+        recipes: RecipeModel[],
+        ratingsMap: Record<string, number[]>,
+    ): AsyncGenerator<{ id: string; avgRating: number }> {
+        for (const recipe of recipes) {
+            const ratings = ratingsMap[recipe.id] || [];
+            if (ratings.length > 0) {
+                const sumRating = ratings.reduce(
+                    (sum, rating) => sum + rating,
+                    0,
+                );
+                const avg = sumRating / ratings.length;
+                yield { id: recipe.id, avgRating: avg };
+            }
         }
     }
 }
