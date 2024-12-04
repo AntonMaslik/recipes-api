b5f6fdbcbfd647504f25f1b3017d555dd0e3345f
AntonMaslik <maslik.fortech@gmail.com>
2024-12-04 16:42:11 +0300

    feature: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.use(cookieParser());
+    app.enableCors({
+        origin: true,
+        credentials: true,
+    });
 
     await app.listen(process.env.PORT ?? 3000);
 }
